789d9bd4769529ed3613414abc0966abd1ac9e15
jnbreid <56651766+jnbreid@users.noreply.github.com>
2025-04-27 17:56:59 +0000

    table structures

---
+++ src/main/java/com/example/todoapp/task/Task.java
@@ -0,0 +1,62 @@
+package com.example.todoapp.task;
+
+import java.time.LocalDate;
+
+public class Task {
+
+    private Long id;
+
+    private String name;
+    private LocalDate deadline;
+    private Integer priority;
+    private Boolean completed;
+    private Long userId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        this.deadline = deadline;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public Boolean getCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(Boolean completed) {
+        this.completed = completed;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
