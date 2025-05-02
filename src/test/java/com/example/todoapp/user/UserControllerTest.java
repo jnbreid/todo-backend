86eadbe0872d026e208fd8d16d2d32eece1e71d9
jnbreid <56651766+jnbreid@users.noreply.github.com>
2025-05-02 18:06:42 +0000

    usercontorller testing started

--- src/test/java/com/example/todoapp/user/UserControllerTest.java
+++ src/test/java/com/example/todoapp/user/UserControllerTest.java
@@ -1,14 +1,26 @@
 package com.example.todoapp.user;
 
 
+import com.example.todoapp.config.SecurityConfig;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
 
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @WebMvcTest(UserController.class)
+@Import(SecurityConfig.class)
+@AutoConfigureMockMvc(addFilters = false)
 public class UserControllerTest {
 
     @Autowired
@@ -20,5 +32,17 @@ public class UserControllerTest {
     private ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
-    void registerUser_Created
+    void registerUserTest() throws Exception{
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUsername("username");
+        userDTO.setPassword("password");
+
+        mockMvc.perform(post("/api/users/register")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userDTO)))
+                .andExpect(status().isCreated());
+
+        verify(userService).registerUser(any(User.class));
+
+    }
 }
