7e72b1621718aa098b10e454f1bd7a034674a24c
jnbreid <56651766+jnbreid@users.noreply.github.com>
2025-05-02 17:47:48 +0000

    tests for userservice class

--- src/test/java/com/example/todoapp/user/UserControllerTest.java
+++ src/test/java/com/example/todoapp/user/UserControllerTest.java
@@ -1,4 +1,24 @@
 package com.example.todoapp.user;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+@WebMvcTest(UserController.class)
 public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private UserService userService;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void registerUser_Created
 }
