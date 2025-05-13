95fbfbb7d7639515a09cbdcaec5d1f7fe448fbc1
jnbreid <56651766+jnbreid@users.noreply.github.com>
2025-05-13 14:25:24 +0000

    unused import

--- src/main/java/com/example/todoapp/task/controller/TaskMapper.java
+++ src/main/java/com/example/todoapp/task/controller/TaskMapper.java
@@ -3,7 +3,6 @@ package com.example.todoapp.task.controller;
 import com.example.todoapp.task.Task;
 import com.example.todoapp.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
