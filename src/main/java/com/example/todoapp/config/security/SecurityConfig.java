44d5e2ad21de8e67e9fc0dca0b5fbf2a9ad8f57c
jnbreid <56651766+jnbreid@users.noreply.github.com>
2025-06-10 20:00:49 +0000

    removed port 81

--- src/main/java/com/example/todoapp/config/security/SecurityConfig.java
+++ src/main/java/com/example/todoapp/config/security/SecurityConfig.java
@@ -35,8 +35,11 @@ public class SecurityConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods(CorsConfiguration.ALL);
-        registry.addMapping("/**").allowedOrigins("http://localhost:8081").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+        //registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods(CorsConfiguration.ALL);
+        registry.addMapping("/**").allowedOrigins("http://localhost:8081")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 
 
