bf9184017593401f0a535951edb9bf6907213611
jnbreid <56651766+jnbreid@users.noreply.github.com>
2025-07-16 18:08:04 +0000

    working config and first test

--- src/test/java/com/example/todoapp/user/controller/UserControllerTest.java
+++ src/test/java/com/example/todoapp/user/controller/UserControllerTest.java
@@ -3,58 +3,63 @@
  * Copyright (c) 2025 Jon Breid
  */
 
+
 package com.example.todoapp.user.controller;
+
 
 import com.example.todoapp.config.RestTestConfig;
 import com.example.todoapp.config.security.CustomUserDetailsService;
 import com.example.todoapp.config.security.JwtTokenUtil;
 import com.example.todoapp.config.security.SecurityConfig;
-import com.example.todoapp.user.User;
 import com.example.todoapp.user.service.UserService;
-import org.junit.jupiter.api.BeforeAll;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(UserController.class)
+@WebMvcTest(controllers = UserController.class,
+        excludeFilters = @ComponentScan.Filter(
+        type  = FilterType.ASSIGNABLE_TYPE,
+        classes = SecurityConfig.class
+))
 @Import(RestTestConfig.class)
-@AutoConfigureMockMvc(addFilters = false)
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockitoBean
+    private AuthenticationManager authenticationManager;
+    @MockitoBean
+    private CustomUserDetailsService customUserDetailsService;
+    @MockitoBean
+    private JwtTokenUtil jwtTokenUtil;
 
     @MockitoBean
     private UserService userService;
     @MockitoBean
-    private AuthenticationManager authenticationManager;
-    @MockitoBean
-    private CustomUserDetailsService userDetailsService;
-    @MockitoBean
-    private JwtTokenUtil jwtTokenUtil;
-
-    private final ObjectMapper objectMapper = new ObjectMapper();
-
-    private final String username = "username";
-    private final String password = "password";
-
+    private UserMapper userMapper;
 
     @Test
     void loginTest_Success() throws Exception{
@@ -62,82 +67,35 @@ public class UserControllerTest {
         userDTO.setUsername("testuser");
         userDTO.setPassword("password");
 
-        User user = new User();
-        user.setUsername("testuser");
-        user.setPassword("password");
+        Authentication auth = new UsernamePasswordAuthenticationToken("testuser", "password", List.of());
+        when(authenticationManager.authenticate(any())).thenReturn(auth);
 
-        when(userService.authenticate(userDTO.getUsername(), userDTO.getPassword()))
-                .thenReturn(user);
+        UserDetails userDetails = org.springframework.security.core.userdetails.User
+                    .withUsername("testuser")
+                    .password("password")
+                    .roles("USER")
+                    .build();
+        when(customUserDetailsService.loadUserByUsername("testuser"))
+                .thenReturn(userDetails);
+
+
+        // fake token 'generated one' from jwt util
+        String fakeToken = "fake-jwt-token";
+        when(jwtTokenUtil.generateToken(userDetails))
+                .thenReturn(fakeToken);
+
 
         mockMvc.perform(post("/api/users/login")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(userDTO)))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userDTO))
+                        .with(csrf())
+                )
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.token").exists());
-    }
-
-    @Test
-    void loginTest_Failure() throws Exception {
-        UserDTO userDTO = new UserDTO();
-        userDTO.setUsername("testuser");
-        userDTO.setPassword("wrongPassword");
-
-        when(userService.authenticate(userDTO.getUsername(), userDTO.getPassword()))
-                .thenThrow(new BadCredentialsException("Invalid password or username."));
-
-        mockMvc.perform(post("/api/users/login")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(userDTO)))
-                .andExpect(status().isUnauthorized());
-    }
-
-    @Test
-    void register_InvalidInputs() throws Exception {
-        String invalidUsername = "a".repeat(62);
-        UserDTO userDTO = new UserDTO();
-        userDTO.setUsername(invalidUsername);
-        userDTO.setPassword("password");
-
-        doThrow(new IllegalArgumentException("Username to long. 60 characters max."))
-                .when(userService).registerUser(any(User.class));
-
-        mockMvc.perform(post("/api/users/register")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(userDTO)))
-                        .andExpect(status().isBadRequest())
-                        .andExpect(content().string("Username to long. 60 characters max."));
-
-        verify(userService).registerUser(any(User.class));
-    }
-
-    @Test
-    void register_OtherException() throws Exception {
-        UserDTO userDTO = new UserDTO();
-        userDTO.setUsername("username");
-        userDTO.setPassword("password");
-
-        doThrow(new RuntimeException())
-                .when(userService).registerUser(any(User.class));
-
-        mockMvc.perform(post("/api/users/register")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(userDTO)))
-                .andExpect(status().isInternalServerError())
-                .andExpect(content().string("An unexpected error occurred."));
-    }
-
-    @Test
-    void registerUserTest() throws Exception{
-        UserDTO userDTO = new UserDTO();
-        userDTO.setUsername("username");
-        userDTO.setPassword("password");
-
-        mockMvc.perform(post("/api/users/register")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(userDTO)))
-                .andExpect(status().isCreated());
-
-        verify(userService).registerUser(any(User.class));
+                .andExpect(jsonPath("$.token").value(fakeToken))
+                .andExpect(jsonPath("$.username").value("testuser"));
 
     }
+
+
+
 }
